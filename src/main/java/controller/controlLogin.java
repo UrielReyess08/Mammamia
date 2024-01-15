63c68eff5b9250a9aad6505e87b631c8f28df799
RefinedCandle49 <valdezvargaskevin@gmail.com>
2024-01-14 20:08:36 -0500

    New changes on controlLogin.java

--- src/main/java/controller/controlLogin.java
+++ src/main/java/controller/controlLogin.java
@@ -1,39 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package controller;
+
+import java.io.*;
 
 import dao.ClienteDao;
 import dao.UsuarioDao;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import java.io.IOException;
-import java.io.PrintWriter;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
 import model.Cliente;
 import model.Usuario;
 
-/**
- *
- * @author daiko
- */
+@WebServlet(name = "controlLogin", value = "/controlLogin")
 public class controlLogin extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         // Validación para invitados
         String action = request.getParameter("action");
 
@@ -102,16 +83,31 @@ public class controlLogin extends HttpServlet {
                 response.sendRedirect(request.getContextPath() + "/customer/login.jsp");
             }
         }
+
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet direcServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet direcServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
     /**
      * Handles the HTTP <code>GET</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -122,10 +118,10 @@ public class controlLogin extends HttpServlet {
     /**
      * Handles the HTTP <code>POST</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
